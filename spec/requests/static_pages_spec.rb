03524c424d85779b7d2ceda07753996365190699
arialblack14 <tsirasalexis@yahoo.gr>
2015-11-12 17:46:07 +0200

    Eliminate repetition (solves exercise 3.1)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 describe "Static pages" do
+  let(:base_title) { "| Ruby on Rails Tutorial Sample App" }
   describe "Home page", type: :feature do
     
     it "has the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "has the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_selector('h1', text: 'Sample App')
     end
 
     it "has the title 'Home'"do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home | Ruby on Rails Tutorial Sample App")
+      visit root_path
+      expect(page).to have_title("Home #{base_title}")
     end
   end
 
@@ -32,7 +33,7 @@ describe "Static pages" do
 
     it "has the title 'Help'"do
       visit '/static_pages/help'
-      expect(page).to have_title("Help | Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("Help #{base_title}")
     end
   end
 
@@ -49,7 +50,7 @@ describe "Static pages" do
 
     it "has the title 'About'"do
       visit '/static_pages/about'
-      expect(page).to have_title("About | Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title("About #{base_title}")
     end
   end
 end
